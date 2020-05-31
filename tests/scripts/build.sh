b9f3c8e9f4d5f9aeb0d42ef52f3fd0ab70e15b41
mufeili <mufeili1996@gmail.com>
2020-06-01 03:21:02 +0800

    Fix path

--- tests/scripts/build.sh
+++ tests/scripts/build.sh
@@ -14,15 +14,17 @@ set -e
 
 rm -rf _deps
 conda activate "pytorch-ci"
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib
 export PYTHONPATH=${PWD}/python:$PYTHONPATH
+
 if [ "$dev" == "gpu" ]; then
-  pip3 uninstall -y dgl
-  pip3 install --pre dgl
+  pip uninstall -y dgl
+  pip install --pre dgl
   pushd python
   python3 setup.py install
 else
-  pip3 uninstall -y dgl-cu101
-  pip3 install --pre dgl-cu101
+  pip uninstall -y dgl-cu101
+  pip install --pre dgl-cu101
   pushd python
   python3 setup.py install
 fi
