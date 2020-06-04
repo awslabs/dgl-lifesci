78da2d6b3dcb773161225679e298a23a9bad2533
mufeili <mufeili1996@gmail.com>
2020-06-04 21:45:34 +0800

    MLP Readout

--- python/dgllife/model/readout/__init__.py
+++ python/dgllife/model/readout/__init__.py
@@ -2,7 +2,8 @@
 #
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
+#
+# Readout functions for computing molecular representations
+# out of node and edge representations.
 
-from .gnn import *
-from .readout import *
-from .model_zoo import *
+from .mlp_readout import *
