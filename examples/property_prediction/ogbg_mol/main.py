1a509354c63080439a9dca5aef37e7668ca807d4
yxgo <58209062+yxgu2353@users.noreply.github.com>
2020-06-30 19:09:38 +0800

    Create main.py
    ogbg_mol

---
+++ examples/property_prediction/ogbg_mol/main.py
@@ -0,0 +1,224 @@
+import argparse
+import numpy as np
+import time
+import torch
+import torch.optim as optim
+
+from dgllife.model import GNNOGBPredictor
+from ogb.graphproppred import DglGraphPropPredDataset, Evaluator, collate_dgl
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+cls_criterion = torch.nn.BCEWithLogitsLoss()
+reg_criterion = torch.nn.MSELoss()
+
+
+def train(gnn, model, feature, device, loader, optimizer, task_type):
+    model.train()
+
+    for step, batch in enumerate(tqdm(loader, desc="Iteration")):
+        bg, labels = batch
+        if bg.batch_size == 1:
+            continue
+
+        nfeats = bg.ndata['feat']
+        efeats = bg.edata['feat']
+        if feature == 'simple':
+            # only retain the top two node/edge features
+            nfeats = nfeats[:, :2]
+            efeats = efeats[:, :2]
+
+        labels, nfeats, efeats = labels.to(device), nfeats.to(device), efeats.to(device)
+        if gnn == 'gcn' or 'gcn-virtual':
+            degs = (bg.in_degrees().float() + 1).to(device)
+          # degs = bg.in_degrees().float().clamp(min=1).to(device)
+        else:
+            degs = None
+        pred = model(bg, nfeats, efeats, degs)
+        optimizer.zero_grad()
+        is_valid = labels == labels
+
+        if "classification" in task_type:
+            loss = cls_criterion(pred.to(torch.float32)[is_valid],
+                                 labels.to(torch.float32)[is_valid])
+        else:
+            loss = reg_criterion(pred.to(torch.float32)[is_valid],
+                                 labels.to(torch.float32)[is_valid])
+
+        loss.backward()
+        optimizer.step()
+
+
+def eval(gnn, model, feature, device, loader, evaluator, task_type):
+    model.eval()
+    y_true = []
+    y_pred = []
+
+    for step, batch in enumerate(tqdm(loader, desc="Iteration")):
+        bg, labels = batch
+        if bg.batch_size == 1:
+            continue
+
+        nfeats = bg.ndata['feat']
+        efeats = bg.edata['feat']
+
+        if feature == 'simple':
+            # only retain the top two node/edge features
+            nfeats = nfeats[:, :2]
+            efeats = efeats[:, :2]
+
+        nfeats, efeats = nfeats.to(device), efeats.to(device)
+        if gnn == 'gcn' or 'gcn-virtual':
+            degs = (bg.in_degrees().float() + 1).to(device)
+            #degs = bg.in_degrees().float().clamp(min=1).to(device)
+        else:
+            degs = None
+
+        with torch.no_grad():
+            pred = model(bg, nfeats, efeats, degs)
+
+        y_true.append(labels.view(pred.shape))
+        y_pred.append(pred.detach().cpu())
+
+    y_true = torch.cat(y_true, dim=0).numpy()
+    if "classification" in task_type:
+        y_pred = torch.sigmoid(torch.cat(y_pred, dim=0)).numpy()
+    else:
+        y_pred = torch.cat(y_pred, dim=0).numpy()
+
+    input_dict = {"y_true": y_true, "y_pred": y_pred}
+
+    return evaluator.eval(input_dict)
+
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='GNN baselines on ogbg-mol* data with DGL')
+    parser.add_argument('--device', type=int, default=0,
+                        help='which gpu to use if any (default: 0)')
+    parser.add_argument('--gnn', type=str, default='gcn',
+                        help='gin, or gin-virtual, or gcn, or gcn-virtual (default: gin-virtual)')
+    parser.add_argument('--drop_ratio', type=float, default=0.5,
+                        help='dropout ratio (default: 0.5)')
+    parser.add_argument('--n_layers', type=int, default=5,
+                        help='number of GNN message passing layers (default: 5)')
+    parser.add_argument('--n_hidden', type=int, default=300,
+                        help='dimensionality of hidden units in GNNs (default: 300)')
+    parser.add_argument('--batch_size', type=int, default=32,
+                        help='input batch size for training (default: 32)')
+    parser.add_argument('--epochs', type=int, default=100,
+                        help='number of epochs to train (default: 100)')
+    parser.add_argument('--num_workers', type=int, default=0,
+                        help='number of workers (default: 0)')
+    parser.add_argument('--dataset', type=str, default="ogbg-molhiv",
+                        help='dataset name (default: ogbg-molhiv)')
+    parser.add_argument('--graph_pooling_type', type=str,
+                        default="mean", choices=["sum", "mean", "max"],
+                        help='type of graph pooling: sum, mean or max')
+    parser.add_argument('--feature', type=str, default="full", choices=["full", "simple"],
+                        help='full feature or simple feature')
+    parser.add_argument('--filename', type=str, default="gin-virtual.pth",
+                        help='filename to output result (default: gin-virtual.pth)')
+    args = parser.parse_args()
+
+    if torch.cuda.is_available():
+        device = torch.device("cuda:" + str(args.device))
+        torch.cuda.set_device(args.device)
+    else:
+        device = torch.device("cpu")
+
+    ### dataloading and splitting
+    dataset = DglGraphPropPredDataset(name=args.dataset)
+    print('Metric', dataset.eval_metric)
+    if args.feature == 'simple':
+        print('using simple feature')
+    else:
+        print('using full feature')
+
+    splitted_idx = dataset.get_idx_split()
+
+    ### automatic evaluator. takes dataset name as input
+    evaluator = Evaluator(args.dataset)
+
+    # using collate_dgl
+    train_loader = DataLoader(dataset[splitted_idx["train"]], batch_size=args.batch_size,
+                              shuffle=True, collate_fn=collate_dgl, num_workers=args.num_workers)
+    valid_loader = DataLoader(dataset[splitted_idx["valid"]], batch_size=args.batch_size,
+                              shuffle=False, collate_fn=collate_dgl, num_workers=args.num_workers)
+    test_loader = DataLoader(dataset[splitted_idx["test"]], batch_size=args.batch_size,
+                             shuffle=False, collate_fn=collate_dgl, num_workers=args.num_workers)
+
+    if args.gnn == 'gin':
+        gnn_type = 'gin'
+        virtual_node = False
+    if args.gnn == 'gcn':
+        gnn_type = 'gcn'
+        virtual_node = False
+    if args.gnn == 'gin-virtual':
+        gnn_type = 'gin'
+        virtual_node = True
+    if args.gnn == 'gcn-virtual':
+        gnn_type = 'gcn'
+        virtual_node = True
+
+    model = GNNOGBPredictor(in_edge_feats=dataset[0][0].edata['feat'].shape[-1],
+                            hidden_feats=args.hidden_feats,
+                            n_layers=args.n_layers,
+                            n_tasks=int(dataset.num_classes),
+                            dropout=args.dropout,
+                            gnn_type=gnn_type,
+                            virtual_node=virtual_node).to(device)
+
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    model = model.to(device)
+
+
+    valid_curve = []
+    test_curve = []
+    train_curve = []
+    time_curve = []
+
+    for epoch in range(1, args.epochs + 1):
+        print("=====Epoch {}".format(epoch))
+        print('Training...')
+        t0 = time.time()
+        train(args.gnn, model, args.feature, device,
+              train_loader, optimizer, dataset.task_type)
+        if epoch >= 3:
+            t1 = time.time()
+            time_curve.append(t1 - t0)
+            print('Training Time: ', time_curve[-1])
+
+        print('Evaluating...')
+        train_perf = eval(args.gnn, model, args.feature, device,
+                          train_loader, evaluator, dataset.task_type)
+        valid_perf = eval(args.gnn, model, args.feature, device,
+                          valid_loader, evaluator, dataset.task_type)
+        test_perf = eval(args.gnn, model, args.feature, device,
+                         test_loader, evaluator, dataset.task_type)
+
+        print({'Train': train_perf, 'Validation': valid_perf, 'Test': test_perf})
+        # dataset.eval_metric = 'rocauc'
+        train_curve.append(train_perf[dataset.eval_metric])
+        valid_curve.append(valid_perf[dataset.eval_metric])
+        test_curve.append(test_perf[dataset.eval_metric])
+
+    if 'classification' in dataset.task_type:
+        best_val_epoch = np.argmax(np.array(valid_curve))
+        best_train = max(train_curve)
+    else:
+        best_val_epoch = np.argmin(np.array(valid_curve))
+        best_train = min(train_curve)
+
+    print('Finished training!')
+    print('Best validation score: {}'.format(valid_curve[best_val_epoch]))
+    print('Test score: {}'.format(test_curve[best_val_epoch]))
+    print('Avg Training Time: ', np.mean(time_curve))
+
+    if not args.filename == '':
+        torch.save({'Val': valid_curve[best_val_epoch], 'Test': test_curve[best_val_epoch],
+                    'Train': train_curve[best_val_epoch], 'BestTrain': best_train}, args.filename)
+
+
+if __name__ == "__main__":
+    main()
