4cb77658e20e5b0213677ff4e2a20592defc8d80
mufeili <mufeili1996@gmail.com>
2020-07-14 10:16:26 +0800

    Fix

--- examples/property_prediction/csv_data_configuration/hyper.py
+++ examples/property_prediction/csv_data_configuration/hyper.py
@@ -39,9 +39,9 @@ weave_hyperparameters = {
 
 mpnn_hyperparameters = {
     'node_out_feats': hp.choice('node_out_feats', [32, 64, 128, 256]),
-    'edge_hidden_feats': hp.choice('edge_hidden_feats', [32, 64, 128]),
+    'edge_hidden_feats': hp.choice('edge_hidden_feats', [32, 64]),
     'num_step_message_passing': hp.choice('num_step_message_passing', [1, 2, 3, 4, 5]),
-    'num_step_set2set': hp.choice('num_step_set2set', [1, 2, 3, 4, 5]),
+    'num_step_set2set': hp.choice('num_step_set2set', [1, 2, 3, 4]),
     'num_layer_set2set': hp.choice('num_layer_set2set', [1, 2, 3])
 }
 
