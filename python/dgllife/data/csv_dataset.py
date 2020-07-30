84e7eff51b9b175906cf099ee492862db0d12fef
mufeili <mufeili1996@gmail.com>
2020-07-31 01:35:02 +0800

    Fix

--- python/dgllife/data/csv_dataset.py
+++ python/dgllife/data/csv_dataset.py
@@ -189,6 +189,6 @@ class MoleculeCSVDataset(object):
             self._task_pos_weights = torch.zeros(self.labels.shape[1])
             num_pos = F.sum(self.labels[indices], dim=0)
             num_indices = F.sum(self.mask[indices], dim=0)
-            self._task_pos_weights[num_indices > 0] = \
-                ((num_indices - num_pos) / num_pos)[num_indices > 0]
+            self._task_pos_weights[num_pos > 0] = \
+                ((num_indices - num_pos) / num_pos)[num_pos > 0]
         return self._task_pos_weights
