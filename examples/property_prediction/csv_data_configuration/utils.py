06f57d1f420477e64bb675a1403d13e291a05419
mufeili <mufeili1996@gmail.com>
2020-08-23 22:49:47 +0800

    Fix

--- examples/property_prediction/csv_data_configuration/utils.py
+++ examples/property_prediction/csv_data_configuration/utils.py
@@ -61,14 +61,8 @@ def init_featurizer(args):
     return args
 
 def load_dataset(args, df):
-    if args['model'] in ['gin_supervised_contextpred', 'gin_supervised_infomax',
-                         'gin_supervised_edgepred', 'gin_supervised_masking']:
-        self_loop = True
-    else:
-        self_loop = False
-
     dataset = MoleculeCSVDataset(df=df,
-                                 smiles_to_graph=partial(smiles_to_bigraph, add_self_loop=self_loop),
+                                 smiles_to_graph=partial(smiles_to_bigraph, add_self_loop=True),
                                  node_featurizer=args['node_featurizer'],
                                  edge_featurizer=args['edge_featurizer'],
                                  smiles_column=args['smiles_column'],
