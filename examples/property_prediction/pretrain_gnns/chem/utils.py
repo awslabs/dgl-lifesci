858dceec9cb5abcb3bea55e51a6285bf918a390d
Wenxuan Fan <44895830+wenx00@users.noreply.github.com>
2021-01-02 12:28:57 +0800

    Bugfix

--- examples/property_prediction/pretrain_gnns/chem/utils.py
+++ examples/property_prediction/pretrain_gnns/chem/utils.py
@@ -78,8 +78,10 @@ class PretrainDataset(object):
     used for pretrain_masking(task=masking) and pretrain_supervised(task=supervised) task.
     """
 
-    def __init__(self, data, smiles_to_graph, node_featurizer, edge_featurizer, task=None):
+    def __init__(self, data, smiles_to_graph, node_featurizer, edge_featurizer, smiles_column, task=None):
         self.data = data
+        self.smiles_column = smiles_column
+        self.smiles = self.df[self.smiles_column].tolist()
         self.smiles_to_graph = smiles_to_graph
         self.node_featurizer = node_featurizer
         self.edge_featurizer = edge_featurizer
