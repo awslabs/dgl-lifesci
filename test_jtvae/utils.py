df3610c6a00e12a3dba40e169dbc1b8b915be269
mufeili <mufeili1996@gmail.com>
2021-01-12 16:39:38 +0800

    Update

--- test_jtvae/utils.py
+++
@@ -1,8 +0,0 @@
-from dgllife.data import JTVAEDataset
-from dgllife.utils import JTVAEVocab
-
-def test_data():
-    vocab = JTVAEVocab()
-    dataset = JTVAEDataset('test.txt', vocab, training=True)
-
-    return vocab, dataset
