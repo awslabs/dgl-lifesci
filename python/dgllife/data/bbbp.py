d5d02e0cf96ca8541eada280645a01d5c1714add
mufeili <mufeili1996@gmail.com>
2022-06-11 17:51:10 +0800

    Update

--- python/dgllife/data/bbbp.py
+++ python/dgllife/data/bbbp.py
@@ -10,7 +10,6 @@ import pandas as pd
 from dgl.data.utils import get_download_dir, download, _get_dgl_url, extract_archive
 
 from .csv_dataset import MoleculeCSVDataset
-from ..utils.mol_to_graph import smiles_to_bigraph
 
 __all__ = ['BBBP']
 
@@ -33,8 +32,8 @@ class BBBP(MoleculeCSVDataset):
     Parameters
     ----------
     smiles_to_graph: callable, str -> DGLGraph
-        A function turning a SMILES string into a DGLGraph.
-        Default to :func:`dgllife.utils.smiles_to_bigraph`.
+        A function turning a SMILES string into a DGLGraph. If None, it uses
+        :func:`dgllife.utils.SMILESToBigraph` by default.
     node_featurizer : callable, rdkit.Chem.rdchem.Mol -> dict
         Featurization for nodes like atoms in a molecule, which can be used to update
         ndata for a DGLGraph. Default to None.
@@ -97,7 +96,7 @@ class BBBP(MoleculeCSVDataset):
     tensor([0.7123])
     """
     def __init__(self,
-                 smiles_to_graph=smiles_to_bigraph,
+                 smiles_to_graph=None,
                  node_featurizer=None,
                  edge_featurizer=None,
                  load=False,
