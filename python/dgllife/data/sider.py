de33c08db591713d05767d7717b086662b110c56
mufeili <mufeili1996@gmail.com>
2022-06-11 18:05:27 +0800

    Update

--- python/dgllife/data/sider.py
+++ python/dgllife/data/sider.py
@@ -10,7 +10,6 @@ import pandas as pd
 from dgl.data.utils import get_download_dir, download, _get_dgl_url, extract_archive
 
 from .csv_dataset import MoleculeCSVDataset
-from ..utils.mol_to_graph import smiles_to_bigraph
 
 __all__ = ['SIDER']
 
@@ -28,8 +27,8 @@ class SIDER(MoleculeCSVDataset):
     Parameters
     ----------
     smiles_to_graph: callable, str -> DGLGraph
-        A function turning a SMILES string into a DGLGraph.
-        Default to :func:`dgllife.utils.smiles_to_bigraph`.
+        A function turning a SMILES string into a DGLGraph. If None, it uses
+        :func:`dgllife.utils.SMILESToBigraph` by default.
     node_featurizer : callable, rdkit.Chem.rdchem.Mol -> dict
         Featurization for nodes like atoms in a molecule, which can be used to update
         ndata for a DGLGraph. Default to None.
@@ -53,9 +52,10 @@ class SIDER(MoleculeCSVDataset):
 
     >>> import torch
     >>> from dgllife.data import SIDER
-    >>> from dgllife.utils import smiles_to_bigraph, CanonicalAtomFeaturizer
+    >>> from dgllife.utils import SMILESToBigraph, CanonicalAtomFeaturizer
 
-    >>> dataset = SIDER(smiles_to_bigraph, CanonicalAtomFeaturizer())
+    >>> smiles_to_g = SMILESToBigraph(node_featurizer=CanonicalAtomFeaturizer())
+    >>> dataset = SIDER(smiles_to_g)
     >>> # Get size of the dataset
     >>> len(dataset)
     1427
@@ -81,7 +81,7 @@ class SIDER(MoleculeCSVDataset):
              0.5060,  0.1136,  0.5106])
     """
     def __init__(self,
-                 smiles_to_graph=smiles_to_bigraph,
+                 smiles_to_graph=None,
                  node_featurizer=None,
                  edge_featurizer=None,
                  load=False,
